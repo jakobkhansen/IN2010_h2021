1ce491f73eb8cf2b4cc4046d9af6b6a64f029a4e
Jakob Hansen <jakob.hansen@hotmail.no>
2021-08-31 23:17:11 +0200

    Add BST printer

--- uke1/kode/BinarySearchTree.py
+++ uke1/kode/BinarySearchTree.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+
 class Node:
     def __init__(self, value : int) -> None:
         self.value : int = value
@@ -11,11 +12,20 @@ class Node:
         right_val = str(self.right.value) if self.right != None else "None"
 
         return f"Node({self.value}, {left_val}, {right_val})"
+    
+    def visualize(self):
+        display(self)
 
 class BinarySearchTree:
 
     def __init__(self):
         self.root = None
+
+    def visualize(self):
+        if self.root != None:
+            self.root.visualize()
+        else:
+            print("Empty tree")
 
     def insert(self, value : int) -> None:
         if self.root == None:
@@ -60,9 +70,61 @@ class BinarySearchTree:
             return self.search_recursive(value, current.right)
 
 
+def display(node):
+    lines, *_ = _display_aux(node)
+    for line in lines:
+        print(line)
+
+def _display_aux(node):
+    """Returns list of strings, width, height, and horizontal coordinate of the root."""
+    # No child.
+    if node.right is None and node.left is None:
+        line = '%s' % node.value
+        width = len(line)
+        height = 1
+        middle = width // 2
+        return [line], width, height, middle
+
+    # Only left child.
+    if node.right is None:
+        lines, n, p, x = _display_aux(node.left)
+        s = '%s' % node.value
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+        second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+        shifted_lines = [line + u * ' ' for line in lines]
+        return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+    # Only right child.
+    if node.left is None:
+        lines, n, p, x = _display_aux(node.right)
+        s = '%s' % node.value
+        u = len(s)
+        first_line = s + x * '_' + (n - x) * ' '
+        second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+        shifted_lines = [u * ' ' + line for line in lines]
+        return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+    # Two children.
+    left, n, p, x = _display_aux(node.left)
+    right, m, q, y = _display_aux(node.right)
+    s = '%s' % node.value
+    u = len(s)
+    first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+    second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+    if p < q:
+        left += [n * ' '] * (q - p)
+    elif q < p:
+        right += [m * ' '] * (p - q)
+    zipped_lines = zip(left, right)
+    lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+    return lines, n + m + u, max(p, q) + 2, n + u // 2
 
 
 bst = BinarySearchTree()
 bst.insert(10)
 bst.insert(15)
 bst.insert(12)
+bst.insert(7)
+
+bst.visualize()
